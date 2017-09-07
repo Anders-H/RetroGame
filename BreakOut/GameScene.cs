cd032ebfca3a2142100cf04117224ce2d1105d88
Anders Hesselbom <anders.hesselbom@nethouse.se>
2017-09-07 13:18:10 +0200

    Example game has moving ball.

--- BreakOut/GameScene.cs
+++ BreakOut/GameScene.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using RetroGame.Input;
 
 namespace BreakOut
 {
     public class GameScene : RetroGame.Scene
     {
+        private KeyboardStateChecker Keyboard { get; } = new KeyboardStateChecker();
         private Bat Bat { get; } = new Bat();
         private Ball Ball { get; } = new Ball();
         public GameScene(RetroGame.RetroGame retroGame) : base(retroGame)
@@ -17,6 +16,9 @@ namespace BreakOut
         }
         public override void Update(GameTime gameTime)
         {
+            Keyboard.UpdateState();
+            Bat.Move(Keyboard);
+            Ball.Move(0, 0, 316, 196);
         }
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
@@ -39,6 +41,21 @@ namespace BreakOut
             X = 0;
             Y = 50;
         }
+        public void Move(KeyboardStateChecker keyboard)
+        {
+            if (keyboard.IsKeyDown(Keys.Up))
+            {
+                Y -= 2;
+                if (Y < 0)
+                    Y = 0;
+            }
+            else if (keyboard.IsKeyDown(Keys.Down))
+            {
+                Y += 2;
+                if (Y > 170)
+                    Y = 170;
+            }
+        }
         public override void Draw(SpriteBatch s) => s.Draw(Game1.BatTexture, new Vector2(X, Y), Color.White);
     }
 
@@ -52,6 +69,24 @@ namespace BreakOut
             Y = 60;
             SpeedX = 2;
             SpeedY = 1;
+        }
+
+        public void Move(int left, int top, int right, int bottom)
+        {
+            var nextX = X + SpeedX;
+            var nextY = Y + SpeedY;
+            if (nextX < left || nextX > right)
+            {
+                SpeedX = -SpeedX;
+                nextX = X + SpeedX;
+            }
+            if (nextY < top || nextY > bottom)
+            {
+                SpeedY = -SpeedY;
+                nextY = Y + SpeedY;
+            }
+            X = nextX;
+            Y = nextY;
         }
         public override void Draw(SpriteBatch s) => s.Draw(Game1.BallTexture, new Vector2(X, Y), Color.White);
     }
