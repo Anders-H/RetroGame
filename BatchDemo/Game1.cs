c04f699adc0234d6542b779f2925d648d0f48234
Anders-H <anders@winsoft.se>
2018-05-19 13:04:03 +0200

    Improved sprite batch example.

--- BatchDemo/Game1.cs
+++ BatchDemo/Game1.cs
@@ -4,58 +4,103 @@ using Microsoft.Xna.Framework.Graphics;
 using RetroGameClasses;
 using RetroGameClasses.RetroTextures;
 using RetroGameClasses.Sprites;
+using RetroGameClasses.Text;
 
 namespace BatchDemo
 {
-	public class Game1 : RetroGame
-	{
-		public static RetroTexture Star { get; set; }
-		public Game1() : base(320, 200, RetroDisplayMode.WindowedWithUpscaling) { }
-		protected override void LoadContent()
-		{
-			Star = RetroTexture.ScaffoldSimpleTexture(GraphicsDevice, 1, 1, Color.White);
-			CurrentScene = new StarsScene(this);
-			base.LoadContent();
-		}
-	}
+    public class Game1 : RetroGame
+    {
+        public static RetroTexture Star { get; set; }
+        public Game1() : base(320, 200, RetroDisplayMode.WindowedWithUpscaling) { }
+        protected override void LoadContent()
+        {
+            Star = RetroTexture.ScaffoldSimpleTexture(GraphicsDevice, 1, 1, Color.White);
+            CurrentScene = new StarsScene(this);
+            BackColor = Color.Black;
+            base.LoadContent();
+        }
+    }
 
-	public class BatchSprite : Sprite, IBatchSprite
-	{
-		private static Random Rnd { get; } = new Random();
-		private float SpeedX { get; }
-		public bool IsAlive => true;
-		public BatchSprite()
-		{
-			Y = Rnd.Next(0, 200);
-			SpeedX = (float)(Rnd.NextDouble() * 4);
-		}
-		public void Act()
-		{
-			X -= SpeedX;
-			if (X < 0)
-			{
-				X = 320;
-				Y = Rnd.Next(0, 200);
-			}
-		}
-		public void Draw(SpriteBatch spriteBatch) => Draw(spriteBatch, Game1.Star, 0);
-	}
+    public class BatchSprite : Sprite, IBatchSprite
+    {
+        private static Random Rnd { get; } = new Random();
+        public float SpeedX { get; }
+        private Color Color { get; }
+        public bool IsAlive => true;
+        public BatchSprite()
+        {
+            Y = Rnd.Next(0, 200);
+            SpeedX = (float)(Rnd.NextDouble() * 4);
+            if (SpeedX > 3)
+                Color = ColorPaletteHelper.GetColor(ColorPalette.White);
+            else if (SpeedX > 2)
+                Color = ColorPaletteHelper.GetColor(ColorPalette.LightGrey);
+            else if (SpeedX > 1)
+                Color = ColorPaletteHelper.GetColor(ColorPalette.Grey);
+            else
+                Color = ColorPaletteHelper.GetColor(ColorPalette.DarkGrey);
+        }
+        public void Act()
+        {
+            X -= SpeedX;
+            if (X < 0)
+            {
+                X = 320;
+                Y = Rnd.Next(0, 200);
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch) => Draw(spriteBatch, Game1.Star, 0, Color);
+    }
 
-	public class StarsScene : Scene
-	{
-		private bool _adding = true;
-		private readonly Batch _batch = new Batch();
-		public StarsScene(RetroGame parent) : base(parent) { }
-		public override void Update(GameTime gameTime)
-		{
-			if (_batch.Count < 1000 && _adding)
-				_batch.AppendLast(new BatchSprite());
-			else if (_adding)
-				_adding = false;
-			else if (_batch.Count > 0)
-				_batch.RemoveFirst();
-			_batch.Act();
-		}
-		public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) => _batch.Draw(spriteBatch);
-	}
+    public class StarsScene : Scene
+    {
+        private bool _adding = true;
+        private readonly TextBlock _textBlock = new TextBlock(CharacterSet.Uppercase);
+        private readonly Batch _batch1 = new Batch();
+        private readonly Batch _batch2 = new Batch();
+        private readonly Batch _batch3 = new Batch();
+        private readonly Batch _batch4 = new Batch();
+        public StarsScene(RetroGame parent) : base(parent) { }
+        private int Count => _batch1.Count + _batch2.Count + _batch3.Count + _batch4.Count;
+        public override void Update(GameTime gameTime)
+        {
+            if (Count < 1000 && _adding)
+            {
+                var s = new BatchSprite();
+                if (s.SpeedX > 3)
+                    _batch4.AppendLast(s);
+                else if (s.SpeedX > 2)
+                    _batch3.AppendLast(s);
+                else if (s.SpeedX > 1)
+                    _batch2.AppendLast(s);
+                else
+                    _batch1.AppendLast(s);
+            }
+            else if (_adding)
+                _adding = false;
+            else if (Count > 0)
+            {
+                if (_batch1.Count > 0)
+                    _batch1.RemoveFirst();
+                if (_batch2.Count > 0)
+                    _batch2.RemoveFirst();
+                if (_batch3.Count > 0)
+                    _batch3.RemoveFirst();
+                if (_batch4.Count > 0)
+                    _batch4.RemoveFirst();
+            }
+            _batch1.Act();
+            _batch2.Act();
+            _batch3.Act();
+            _batch4.Act();
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            _batch1.Draw(spriteBatch);
+            _batch2.Draw(spriteBatch);
+            _batch3.Draw(spriteBatch);
+            _batch4.Draw(spriteBatch);
+            _textBlock.DirectDraw(spriteBatch, 0, 0, $"sprites in batch: {Count}", ColorPalette.Yellow);
+        }
+    }
 }
--- RetroGame/Text/TextBlock.cs
+++ RetroGame/Text/TextBlock.cs
@@ -4,72 +4,104 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace RetroGameClasses.Text
 {
-	public class TextBlock
-	{
-		private Petscii?[,] Characters { get; }
-		private CharacterSet CharacterSet { get; }
-		public TextBlock() : this(40, 25, CharacterSet.Lowercase) { }
-		public TextBlock(int columns, int rows, CharacterSet characterSet)
-		{
-			Characters = new Petscii?[columns, rows];
-			CharacterSet = characterSet;
-		}
-		public void SetText(int x, int y, string text)
-		{
-			var startX = x;
-			if (string.IsNullOrEmpty(text))
-				return;
-			foreach (var t in text)
-			{
-				Characters[x, y] = GetCharacter(t);
-				x++;
-				if (x < Characters.GetLength(0))
-					continue;
-				y++;
-				x = startX;
-				if (y >= Characters.GetLength(1))
-					return;
-			}
-		}
-		private Petscii GetCharacter(char c)
-		{
-			switch (c)
-			{
-				case '@': return Petscii.At;
-				case 'a': return Petscii.A;
-				case 'c': return Petscii.C;
-				case 'e': return Petscii.E;
-				case 'f': return Petscii.F;
-				case 'i': return Petscii.I;
-				case 'l': return Petscii.L;
-				case 'o': return Petscii.O;
-				case 'p': return Petscii.P;
-				case 'r': return Petscii.R;
-				case 's': return Petscii.S;
-				case 't': return Petscii.T;
-				case ' ': return Petscii.Space;
-				case '!': return Petscii.Exclamation;
-				case '(': return Petscii.LeftParentheses;
-				case ')': return Petscii.RightParentheses;
-				default: throw new ArgumentOutOfRangeException(c.ToString());
-			}
-		}
-		public void Draw(SpriteBatch spriteBatch, ColorPalette color) =>
-			Draw(spriteBatch, ColorPaletteHelper.GetColor(color));
-		public void Draw(SpriteBatch spriteBatch, Color color)
-		{
-			for (var y = 0; y < Characters.GetLength(1); y++)
-			{
-				for (var x = 0; x < Characters.GetLength(0); x++)
-				{
-					var c = Characters[x, y];
-					if (c == null)
-						continue;
-					var sourceY = ((int)CharacterSet * 64) + ((int)c % 16) * 8;
-					var sourceX = ((int)c / 16) * 8;
-					spriteBatch.Draw(RetroGame.Font64, new Vector2(x * 8, y * 8), new Rectangle(sourceX, sourceY, 8, 8), color);
-				}
-			}
-		}
-	}
+    public class TextBlock
+    {
+        private Petscii?[,] Characters { get; }
+        private CharacterSet CharacterSet { get; }
+        public TextBlock() : this(40, 25, CharacterSet.Lowercase) { }
+        public TextBlock(CharacterSet characterSet) : this(40, 25, characterSet) { }
+        public TextBlock(int columns, int rows, CharacterSet characterSet)
+        {
+            Characters = new Petscii?[columns, rows];
+            CharacterSet = characterSet;
+        }
+        public void SetText(int x, int y, string text)
+        {
+            var startX = x;
+            if (string.IsNullOrEmpty(text))
+                return;
+            foreach (var t in text)
+            {
+                Characters[x, y] = GetCharacter(t);
+                x++;
+                if (x < Characters.GetLength(0))
+                    continue;
+                y++;
+                x = startX;
+                if (y >= Characters.GetLength(1))
+                    return;
+            }
+        }
+        private static Petscii GetCharacter(char c)
+        {
+            switch (c)
+            {
+                case '@': return Petscii.At;
+                case 'a': return Petscii.A;
+                case 'b': return Petscii.B;
+                case 'c': return Petscii.C;
+                case 'e': return Petscii.E;
+                case 'f': return Petscii.F;
+                case 'h': return Petscii.H;
+                case 'i': return Petscii.I;
+                case 'l': return Petscii.L;
+                case 'n': return Petscii.N;
+                case 'o': return Petscii.O;
+                case 'p': return Petscii.P;
+                case 'r': return Petscii.R;
+                case 's': return Petscii.S;
+                case 't': return Petscii.T;
+                case ':': return Petscii.Colon;
+                case '0': return Petscii.Num0;
+                case '1': return Petscii.Num1;
+                case '2': return Petscii.Num2;
+                case '3': return Petscii.Num3;
+                case '4': return Petscii.Num4;
+                case '5': return Petscii.Num5;
+                case '6': return Petscii.Num6;
+                case '7': return Petscii.Num7;
+                case '8': return Petscii.Num8;
+                case '9': return Petscii.Num9;
+                case ' ': return Petscii.Space;
+                case '!': return Petscii.Exclamation;
+                case '(': return Petscii.LeftParentheses;
+                case ')': return Petscii.RightParentheses;
+                default: throw new ArgumentOutOfRangeException(c.ToString());
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch, ColorPalette color) =>
+            Draw(spriteBatch, ColorPaletteHelper.GetColor(color));
+        public void Draw(SpriteBatch spriteBatch, Color color)
+        {
+            for (var y = 0; y < Characters.GetLength(1); y++)
+            {
+                for (var x = 0; x < Characters.GetLength(0); x++)
+                {
+                    var c = Characters[x, y];
+                    if (c == null)
+                        continue;
+                    var sourceX = (int)CharacterSet*64+(int)c/16*8;
+                    var sourceY = (int)c%16*8;
+                    spriteBatch.Draw(RetroGame.Font64, new Vector2(x*8, y*8), new Rectangle(sourceX, sourceY, 8, 8), color);
+                }
+            }
+        }
+        public void DirectDraw(SpriteBatch spriteBatch, int x, int y, string text, ColorPalette color) =>
+            DirectDraw(spriteBatch, x, y, text, ColorPaletteHelper.GetColor(color));
+        public void DirectDraw(SpriteBatch spriteBatch, int x, int y, string text, Color color)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+            for (var i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                if (c == ' ')
+                    continue;
+                var pet = GetCharacter(c);
+                var sourceX = (int)CharacterSet*64 + (int)pet/16*8;
+                var sourceY = (int)pet%16*8;
+                spriteBatch.Draw(RetroGame.Font64, new Vector2(x + i*8, y), new Rectangle(sourceX, sourceY, 8, 8), color);
+            }
+        }
+    }
 }
