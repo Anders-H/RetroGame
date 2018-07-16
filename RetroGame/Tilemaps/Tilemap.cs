e375a60b836e8cb4bee2d8c52f8e89b343435967
Anders-H <anders@winsoft.se>
2018-07-16 10:38:49 +0200

    Work in progress: Tile map.

--- RetroGame/Tilemaps/Tilemap.cs
+++ RetroGame/Tilemaps/Tilemap.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using RetroGameClasses.RetroTextures;
 using RetroGameClasses.Scene;
 
 namespace RetroGameClasses.Tilemaps
 {
-    public class Tilemap : IRetroActor
+    public class Tilemap : ISceneActor
     {
         public RetroTexture CurrentTexture { get; }
         public Point GridSize { get; }
         public Point TileSize { get; }
         public Point DisplaySize { get; }
+        public int PixelOffsetX { get; set; }
+        public int PixelOffsetY { get; set; }
+        public int TileOffsetX { get; set; }
+        public int TileOffsetY { get; set; }
+        public int Delay { get; set; }
+        public int SpeedX { get; set; } = -1;
+        public int SpeedY { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
         public Tilemap(RetroTexture texture, Point gridSize, Point tileSize, Point displaySize)
         {
             CurrentTexture = texture;
@@ -20,6 +30,20 @@ namespace RetroGameClasses.Tilemaps
         public Tilemap(RetroTexture texture, int gridSizeX, int gridSizeY, int tileSizeX, int tileSizeY, int displaySizeX, int displaySizeY) : this(texture, new Point(gridSizeX, gridSizeY), new Point(tileSizeX, tileSizeY), new Point(displaySizeX, displaySizeY)) { }
         public void Act(ulong ticks)
         {
+            if (Delay > 0 && ticks%(ulong)Delay != 0)
+                return;
+            PixelOffsetX += SpeedX;
+            if (PixelOffsetX >= TileSize.X)
+            {
+                PixelOffsetX = 0;
+                TileOffsetX += 1;
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch, ulong ticks)
+        {
+            var x = X + PixelOffsetX;
+            var y = Y + PixelOffsetY;
+
         }
     }
 }
--- TilesDemo/Game1.cs
+++ TilesDemo/Game1.cs
@@ -3,17 +3,21 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using RetroGameClasses;
 using RetroGameClasses.Input;
+using RetroGameClasses.RetroTextures;
 using RetroGameClasses.Scene;
+using RetroGameClasses.Tilemaps;
 
 namespace TilesDemo
 {
     public class Game1 : RetroGame
     {
+        internal Texture2D Tiles { get; set; }
         public Game1() : base(320, 200, RetroDisplayMode.WindowedWithUpscaling)
         {
         }
         protected override void LoadContent()
         {
+            Tiles = Content.Load<Texture2D>("test_tiles");
             BorderColor = ColorPaletteHelper.GetColor(ColorPalette.LightGreen);
             BackColor = ColorPaletteHelper.GetColor(ColorPalette.DarkGrey);
             CurrentScene = new TextureDemoScene(this);
@@ -23,9 +27,14 @@ namespace TilesDemo
     public class TextureDemoScene: Scene
     {
         private KeyboardStateChecker Keyboard { get; } = new KeyboardStateChecker();
+
+        private Tilemap Tiles { get; }
         public TextureDemoScene(RetroGame parent) : base(parent)
         {
-            AddToAutoUpdate(Keyboard);
+            var texture = new RetroTexture(parent.GraphicsDevice, 32, 32, 10);
+            Tiles = new Tilemap(texture, 10, 5, 32, 32, 5, 5);
+            AddToAutoUpdate(Keyboard, Tiles);
+            AddToAutoDraw(Tiles);
         }
         public override void Update(GameTime gameTime, ulong ticks)
         {
@@ -33,10 +42,6 @@ namespace TilesDemo
             if (Keyboard.IsKeyPressed(Keys.Escape))
                 Exit();
             base.Update(gameTime, ticks);
-        }
-        public override void Draw(GameTime gameTime, ulong ticks, SpriteBatch spriteBatch)
-        {
-
         }
     }
 }
