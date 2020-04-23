508c2e4101a5b99602d6fb54c19840c1639a6738
Anders-H <anders@winsoft.se>
2020-04-23 20:11:18 +0200

    CollisionTexture added.

--- RetroGame/RetroTextures/CollisionTexture.cs
+++ RetroGame/RetroTextures/CollisionTexture.cs
@@ -1,7 +1,33 @@
-﻿namespace RetroGameClasses.RetroTextures
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RetroGameClasses.RetroTextures
 {
-    public class CollisionTexture
+    public class CollisionTexture : RetroTexture
     {
+        public List<Rectangle> CollisionZones { get; }
         
+        public CollisionTexture(GraphicsDevice graphicsDevice, Point cellSize, int cellCount) : base(graphicsDevice, cellSize, cellCount)
+        {
+            CollisionZones = new List<Rectangle>();
+        }
+
+        public CollisionTexture(GraphicsDevice graphicsDevice, int cellWidth, int cellHeight, int cellCount) : base(graphicsDevice, cellWidth, cellHeight, cellCount)
+        {
+            CollisionZones = new List<Rectangle>();
+        }
+
+        public bool Intersects(Rectangle rectangle) =>
+            CollisionZones.Any(collisionZone => collisionZone.Intersects(rectangle));
+
+        public bool Intersects(IEnumerable<Rectangle> rectangles) =>
+            (
+                from collisionZone in CollisionZones
+                from rectangle in rectangles
+                where collisionZone.Intersects(rectangle)
+                select collisionZone
+            ).Any();
     }
 }
