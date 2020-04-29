5d27331d31f486b097d7707560929e8578d523bc
Anders-H <anders@winsoft.se>
2020-04-29 21:21:36 +0200

    Location added to platform character collision texture.

--- RetroGame/RetroTextures/PlatformGameCharacterCollisionTexture.cs
+++ RetroGame/RetroTextures/PlatformGameCharacterCollisionTexture.cs
@@ -8,54 +8,65 @@ namespace RetroGameClasses.RetroTextures
     public class PlatformGameCharacterCollisionTexture : RetroTexture
     {
         
-        public Rectangle BodyZone { get; set; }
-        public Rectangle FeetZone { get; set; }
+        public Rectangle[] BodyZone { get; set; }
+        public Rectangle[] FeetZone { get; set; }
         
         public PlatformGameCharacterCollisionTexture(GraphicsDevice graphicsDevice, Point cellSize, int cellCount) : base(graphicsDevice, cellSize, cellCount)
         {
+            BodyZone = new Rectangle[cellCount];
+            FeetZone = new Rectangle[cellCount];
         }
 
         public PlatformGameCharacterCollisionTexture(GraphicsDevice graphicsDevice, int cellWidth, int cellHeight, int cellCount) : base(graphicsDevice, cellWidth, cellHeight, cellCount)
         {
+            BodyZone = new Rectangle[cellCount];
+            FeetZone = new Rectangle[cellCount];
         }
 
-        //TODO: Location!
-        
-        public bool Intersects(Rectangle rectangle) =>
-            BodyZone.Intersects(rectangle)
-            || FeetZone.Intersects(rectangle);
+        public bool Intersects(int cellIndex, Point myLocation, Rectangle rectangle, Point rectangleLocation) =>
+            BodyIntersects(cellIndex, myLocation, rectangle, rectangleLocation)
+            || FeetIntersects(cellIndex, myLocation, rectangle, rectangleLocation);
 
-        public bool Intersects(IEnumerable<Rectangle> rectangles) =>
-            rectangles.Any(
-                rectangle => BodyZone.Intersects(rectangle) || FeetZone.Intersects(rectangle)
+        public bool BodyIntersects(int cellIndex, Point myLocation, Rectangle rectangle, Point rectangleLocation)
+        {
+            var b = BodyZone[cellIndex];
+            
+            var body = new Rectangle(
+                b.X + myLocation.X,
+                b.Y + myLocation.Y,
+                b.Width,
+                b.Height
             );
-        
-        public bool Intersects(PlatformGameCharacterCollisionTexture texture) =>
-            Intersects(texture.BodyZone)
-            || Intersects(texture.FeetZone);
-
-        public bool BodyIntersects(Rectangle rectangle) =>
-            BodyZone.Intersects(rectangle);
-
-        public bool BodyIntersects(IEnumerable<Rectangle> rectangles) =>
-            rectangles.Any(
-                rectangle => BodyZone.Intersects(rectangle)
-            );
-        
-        public bool BodyIntersects(PlatformGameCharacterCollisionTexture texture) =>
-            BodyZone.Intersects(texture.BodyZone)
-            || BodyZone.Intersects(texture.FeetZone);
-        
-        public bool FeetIntersects(Rectangle rectangle) =>
-            FeetZone.Intersects(rectangle);
-
-        public bool FeetIntersects(IEnumerable<Rectangle> rectangles) =>
-            rectangles.Any(
-                rectangle => FeetZone.Intersects(rectangle)
+            
+            var otherRectangle = new Rectangle(
+                rectangle.X + rectangleLocation.X,
+                rectangle.Y + rectangleLocation.Y,
+                rectangle.Width,
+                rectangle.Height
             );
 
-        public bool FeetIntersects(PlatformGameCharacterCollisionTexture texture) =>
-            FeetZone.Intersects(texture.BodyZone)
-            || FeetZone.Intersects(texture.FeetZone);
+            return body.Intersects(otherRectangle);
+        }
+        
+        public bool FeetIntersects(int cellIndex, Point myLocation, Rectangle rectangle, Point rectangleLocation)
+        {
+            var f = FeetZone[cellIndex];
+            
+            var feet = new Rectangle(
+                f.X + myLocation.X,
+                f.Y + myLocation.Y,
+                f.Width,
+                f.Height
+            );
+            
+            var otherRectangle = new Rectangle(
+                rectangle.X + rectangleLocation.X,
+                rectangle.Y + rectangleLocation.Y,
+                rectangle.Width,
+                rectangle.Height
+            );
+
+            return feet.Intersects(otherRectangle);
+        }
     }
 }
