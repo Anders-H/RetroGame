ad97173278d452a10731e2519a7cccaf00891525
Anders Hesselbom <anders@winsoft.se>
2025-05-03 15:37:32 +0200

    Score animation added.

--- RetroGame/Scene/IngameScene.cs
+++ RetroGame/Scene/IngameScene.cs
@@ -1,4 +1,5 @@
-﻿using RetroGame.Input;
+﻿using Microsoft.Xna.Framework;
+using RetroGame.Input;
 using RetroGame.Text;
 
 namespace RetroGame.Scene;
@@ -6,13 +7,15 @@ namespace RetroGame.Scene;
 public abstract class IngameScene : Scene
 {
     private int _score;
+    private int _displayScore;
     private string _scoreString;
+    private bool _scoreInvalidated;
     protected KeyboardStateChecker Keyboard { get; }
     protected readonly TextBlock Text;
 
     protected IngameScene(RetroGame parent) : base(parent)
     {
-        Score = 1; // To provoke the score string to be created
+        _scoreInvalidated = true;
         Score = 0;
         Text = new TextBlock(CharacterSet.Uppercase);
         Keyboard = new KeyboardStateChecker();
@@ -24,29 +27,39 @@ public abstract class IngameScene : Scene
         get => _score;
         set
         {
-            if (value != _score)
-            {
-                _score = value;
-
-                if (_score < 0)
-                    _score = 0;
-
-                var s = _score.ToString();
-
-                _scoreString = s.Length switch
-                {
-                    1 => $"score:        {s}",
-                    2 => $"score:       {s}",
-                    3 => $"score:      {s}",
-                    4 => $"score:     {s}",
-                    5 => $"score:    {s}",
-                    6 => $"score:   {s}",
-                    7 => $"score:  {s}",
-                    8 => $"score: {s}",
-                    _ => $"score:{s}"
-                };
-            }
+            _score = value;
+            _scoreInvalidated = true;
         }
+    }
+
+    public override void Update(GameTime gameTime, ulong ticks)
+    {
+        if (_scoreInvalidated)
+        {
+            if (_score > _displayScore)
+                _displayScore++;
+            else if (_score < _displayScore)
+                _displayScore = _score;
+
+            var s = _displayScore.ToString();
+
+            _scoreString = s.Length switch
+            {
+                1 => $"score:        {s}",
+                2 => $"score:       {s}",
+                3 => $"score:      {s}",
+                4 => $"score:     {s}",
+                5 => $"score:    {s}",
+                6 => $"score:   {s}",
+                7 => $"score:  {s}",
+                8 => $"score: {s}",
+                _ => $"score:{s}"
+            };
+
+            _scoreInvalidated = _score != _displayScore;
+        }
+
+        base.Update(gameTime, ticks);
     }
 
     public string ScoreString =>
