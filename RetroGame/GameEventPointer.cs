22f311220a56f92cceb3e7819572df7a64c76ff4
Anders Hesselbom <anders@winsoft.se>
2025-05-20 19:56:30 +0200

    Added a GameEventPointer.

---
+++ RetroGame/GameEventPointer.cs
@@ -0,0 +1,22 @@
+﻿namespace RetroGame;
+
+public struct GameEventPointer
+{
+    public bool Occured { get; set; }
+    public ulong OccuredAt { get; set; }
+
+    public GameEventPointer()
+    {
+        Occured = false;
+        OccuredAt = 0;
+    }
+
+    public void Occure(ulong ticks)
+    {
+        Occured = true;
+        OccuredAt = ticks;
+    }
+
+    public bool OccuredTicksAgo(ulong ticks, ulong delay) =>
+        Occured && OccuredAt + ticks > delay;
+}
